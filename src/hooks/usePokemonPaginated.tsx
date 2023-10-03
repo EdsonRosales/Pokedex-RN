9867c6a6b7a108195f302503c0ca2d91db2f2969
Edson Rosales <edsonme9@gmail.com>
2023-10-02 20:13:47 -0600

    feat[CustomHook]: Build the paginated reference for the pokeapi & build the type of the data response

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -1,18 +1,29 @@
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { pokemonApi } from '../api/pokemonApi';
+import type { SimplePokemon, PokemonPaginatedResponse, Result } from '../interfaces/pokemonInterfaces';
 
 export const usePokemonPaginated = () => {
-  const url = 'https://pokeapi.co/api/v2/pokemon?limit=40';
+  
+  const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([]);
   const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40')  // <--- Reference to the next page of the api
   
   // Do the request to the pokeapi endpoint
   const loadPokemons = async () => {
-    const resp = await pokemonApi.get(nextPageUrl.current);
-    console.log(resp.data);
+    const resp = await pokemonApi.get<PokemonPaginatedResponse>(nextPageUrl.current);
+    nextPageUrl.current = resp.data.next;   // <---- Pagination of pokemons with the ref
+    mapPokemonListToSimplePokemon(resp.data.results);
+  };
+
+  // Transform the data shape
+  const mapPokemonListToSimplePokemon = ( pokemonList: Result[] ) => {
+    pokemonList.forEach( poke => console.log(poke.name) );
   };
 
   useEffect(() => {
     loadPokemons();
   }, [])
-  
+
+  return {
+    simplePokemonList
+  };
 };
