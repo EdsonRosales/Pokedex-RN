f03df4e42c512649061f1bd1eae97d79e14200cc
Edson Rosales <edsonme9@gmail.com>
2023-10-02 21:16:08 -0600

    feat[ShowPokemons]: Show Pokemons with infinite scroll

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Image, Text } from 'react-native';
-import { styles } from '../theme/appTheme';
+import { ActivityIndicator, FlatList, Image, Text } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
 import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
+import { styles } from '../theme/appTheme';
 
 export const HomeScreen = () => {
 
   const { top } = useSafeAreaInsets();
-  const { simplePokemonList } = usePokemonPaginated();
-  console.log(simplePokemonList);
+  const { simplePokemonList, loadPokemons } = usePokemonPaginated();
 
   return (
     <>
@@ -16,13 +16,41 @@ export const HomeScreen = () => {
         source={ require('../assets/pokebola.png') }
         style={ styles.pokebolaBG }
       />
-      <Text
+
+      <FlatList
+        data={ simplePokemonList }
+        keyExtractor={ (pokemon) => pokemon.id }
+        showsVerticalScrollIndicator={false}
+        renderItem={ ({ item, index }) => (
+          <Image
+            source={{ uri: item.picture }}
+            style={{
+              width: 100,
+              height: 100
+            }}
+          />
+        )}
+
+        // Infinite scroll
+        onEndReached={ loadPokemons }
+        onEndReachedThreshold={ 0.4 }
+
+        ListFooterComponent={( 
+          <ActivityIndicator 
+            style={{ height: 100 }}
+            size={ 20 }
+            color="grey"
+          />
+        )}
+      />
+
+      {/* <Text
         style={{
           ...styles.title,
           ...styles.globalMargin,
           top: top + 20
         }}
-      >Pokedex</Text>
+      >Pokedex</Text> */}
     </>
   )
 };
