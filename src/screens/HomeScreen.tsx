2f995f936fdaf0aa2ab53923bfe30d2d48511043
Edson Rosales <edsonme9@gmail.com>
2023-10-02 18:41:43 -0600

    feat[Design]: Basic design of HomeScreen component & added some global styles into appTheme folder

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import { Text, View } from 'react-native';
+import { Image, Text } from 'react-native';
+import { styles } from '../theme/appTheme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const HomeScreen = () => {
+
+  const { top } = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <>
+      <Image
+        source={ require('../assets/pokebola.png') }
+        style={ styles.pokebolaBG }
+      />
+      <Text
+        style={{
+          ...styles.title,
+          ...styles.globalMargin,
+          top: top + 20
+        }}
+      >Pokedex</Text>
+    </>
   )
 };
