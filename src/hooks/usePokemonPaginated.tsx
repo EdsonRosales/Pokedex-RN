8acd05dcfdcfcc688f20539a256bee75cfdf8b7b
Edson Rosales <edsonme9@gmail.com>
2023-10-02 21:00:30 -0600

    feat[CustomHook]: Map the result to an arrayof SimplePokemon structure

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -4,11 +4,13 @@ import type { SimplePokemon, PokemonPaginatedResponse, Result } from '../interfa
 
 export const usePokemonPaginated = () => {
   
+  const [isLoading, setIsLoading] = useState(true); // <---- Loader
   const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([]);
   const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40')  // <--- Reference to the next page of the api
   
   // Do the request to the pokeapi endpoint
   const loadPokemons = async () => {
+    setIsLoading(true);
     const resp = await pokemonApi.get<PokemonPaginatedResponse>(nextPageUrl.current);
     nextPageUrl.current = resp.data.next;   // <---- Pagination of pokemons with the ref
     mapPokemonListToSimplePokemon(resp.data.results);
@@ -16,7 +18,18 @@ export const usePokemonPaginated = () => {
 
   // Transform the data shape
   const mapPokemonListToSimplePokemon = ( pokemonList: Result[] ) => {
-    pokemonList.forEach( poke => console.log(poke.name) );
+    
+    const newPokemonList: SimplePokemon[] = pokemonList.map(({ name, url }) => {
+      const urlParts = url.split('/');  // <--- Cut the original url by every '/' to create a new array with all parts
+      const id = urlParts[ urlParts.length - 2 ];
+      const picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+
+      return { id, picture, name };
+    });
+
+    // Set the state with the old & the new pokemons with the correct shape of data 'newPokemonList'
+    setSimplePokemonList([ ...simplePokemonList, ...newPokemonList ]);
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -24,6 +37,7 @@ export const usePokemonPaginated = () => {
   }, [])
 
   return {
+    isLoading,
     simplePokemonList
   };
 };
